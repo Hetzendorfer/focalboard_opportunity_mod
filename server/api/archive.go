e251ad7bc08122e1039f08693f483d2b3bf6f2bb
Hetzendorfer, Kevin <kevin.hetzendorfer@accenture.com>
2024-02-06 19:01:33 +0100

    first commit

---
+++ server/api/archive.go
@@ -0,0 +1,570 @@
+package api
+
+import (
+	// "encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/mattermost/focalboard/server/model"
+	"github.com/mattermost/focalboard/server/services/audit"
+	mmModel "github.com/mattermost/mattermost-server/v6/model"
+	"github.com/mattermost/mattermost-server/v6/shared/mlog"
+)
+
+const (
+	archiveExtension = ".boardarchive"
+)
+
+func (a *API) registerAchivesRoutes(r *mux.Router) {
+	// Archive APIs
+	r.HandleFunc("/boards/{boardID}/archive/export", a.sessionRequired(a.handleArchiveExportBoard)).Methods("GET")
+	r.HandleFunc("/boards/{boardID}/archive/import/opportunities", a.sessionRequired(a.handleArchiveImportOpportunities)).Methods("POST")
+	r.HandleFunc("/teams/{teamID}/archive/import", a.sessionRequired(a.handleArchiveImport)).Methods("POST")
+	r.HandleFunc("/teams/{teamID}/archive/export", a.sessionRequired(a.handleArchiveExportTeam)).Methods("GET")
+}
+
+func (a *API) handleArchiveExportBoard(w http.ResponseWriter, r *http.Request) {
+	// swagger:operation GET /boards/{boardID}/archive/export archiveExportBoard
+	//
+	// Exports an archive of all blocks for one boards.
+	//
+	// ---
+	// produces:
+	// - application/json
+	// parameters:
+	// - name: boardID
+	//   in: path
+	//   description: Id of board to export
+	//   required: true
+	//   type: string
+	// security:
+	// - BearerAuth: []
+	// responses:
+	//   '200':
+	//     description: success
+	//     content:
+	//       application-octet-stream:
+	//         type: string
+	//         format: binary
+	//   default:
+	//     description: internal error
+	//     schema:
+	//       "$ref": "#/definitions/ErrorResponse"
+
+	vars := mux.Vars(r)
+	boardID := vars["boardID"]
+	userID := getUserID(r)
+
+	// check user has permission to board
+	if !a.permissions.HasPermissionToBoard(userID, boardID, model.PermissionViewBoard) {
+		// if this user has `manage_system` permission and there is a license with the compliance
+		// feature enabled, then we will allow the export.
+		license := a.app.GetLicense()
+		if !a.permissions.HasPermissionTo(userID, mmModel.PermissionManageSystem) || license == nil || !(*license.Features.Compliance) {
+			a.errorResponse(w, r, model.NewErrPermission("access denied to board"))
+			return
+		}
+	}
+
+	auditRec := a.makeAuditRecord(r, "archiveExportBoard", audit.Fail)
+	defer a.audit.LogRecord(audit.LevelRead, auditRec)
+	auditRec.AddMeta("BoardID", boardID)
+
+	board, err := a.app.GetBoard(boardID)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+
+	opts := model.ExportArchiveOptions{
+		TeamID:   board.TeamID,
+		BoardIDs: []string{board.ID},
+	}
+
+	filename := fmt.Sprintf("archive-%s%s", time.Now().Format("2006-01-02"), archiveExtension)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+
+	if err := a.app.ExportArchive(w, opts); err != nil {
+		a.errorResponse(w, r, err)
+	}
+
+	auditRec.Success()
+}
+
+func (a *API) filterBlocksByOpportunityId(blocks []*model.Block, opportunities *[]Opportunity, boardTemplate *model.Board, templateBlockID string, userID string) []*model.Block {
+	var updatedBlocks []*model.Block
+
+	boardID := boardTemplate.ID
+	possibleValues := boardTemplate.CardProperties
+
+	for _, opportunity := range *opportunities {
+		var found model.Block = model.Block{Title: ""}
+
+		for _, block := range blocks {
+			if opportunity.OpportunityId == block.Fields["properties"].(map[string]interface{})[opportunityIdKey] {
+				found = *block
+				break
+			}
+		}
+
+		if found.Title != "" {
+			duplicatedBlock := found
+			convertOpportunityToBlock(opportunity, &duplicatedBlock, possibleValues)
+			updatedBlocks = append(updatedBlocks, &duplicatedBlock)
+		} else {
+			fmt.Println("Duplicating block else statement")
+			duplicateBlocks, err := a.app.DuplicateBlock(boardID, templateBlockID, userID, true)
+			if err != nil {
+			}
+
+			duplicatedBlock := duplicateBlocks[0]
+			convertOpportunityToBlock(opportunity, duplicatedBlock, possibleValues)
+			updatedBlocks = append(updatedBlocks, duplicatedBlock)
+		}
+	}
+
+	return updatedBlocks
+}
+
+type ReturnTypeABC struct {
+	opportunityId []string
+	blocks        []*model.Block
+}
+
+func (a *API) handleArchiveImportOpportunities(w http.ResponseWriter, r *http.Request) {
+	// swagger:operation POST /boards/{boardID}/archive/import/opportunities
+	//
+	//     post:
+	//         consumes:
+	//             - multipart/form-data
+	//         operationId: importOpportunitiers
+	//         parameters:
+	//             - description: Team ID
+	//               in: path
+	//               name: teamID
+	//               required: true
+	//               type: string
+	//             - description: xlsx file to import
+	//               in: formData
+	//               name: file
+	//               required: true
+	//               type: file
+	//         produces:
+	//             - application/json
+	//         responses:
+	//             "200":
+	//                 description: success
+	//             default:
+	//                 description: internal error
+	//                 schema:
+	//                     $ref: '#/definitions/ErrorResponse'
+	//         security:
+	//             - BearerAuth: []
+	//         summary: Import opportunities to all boards in a team.
+
+	vars := mux.Vars(r)
+	boardID := vars["boardID"]
+	userID := getUserID(r)
+
+	_ = boardID
+
+	// check user has permission to board
+	if !a.permissions.HasPermissionToBoard(userID, boardID, model.PermissionViewBoard) {
+		// if this user has `manage_system` permission and there is a license with the compliance
+		// feature enabled, then we will allow the export.
+		license := a.app.GetLicense()
+		if !a.permissions.HasPermissionTo(userID, mmModel.PermissionManageSystem) || license == nil || !(*license.Features.Compliance) {
+			a.errorResponse(w, r, model.NewErrPermission("access denied to board"))
+			return
+		}
+	}
+
+	isGuest, err := a.userIsGuest(userID)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+	if isGuest {
+		a.errorResponse(w, r, model.NewErrPermission("access denied to create board"))
+		return
+	}
+
+	// retrieve boards list
+	// boards, err := a.app.GetBoardsForUserAndTeam(userID, teamID, !isGuest)
+	// if err != nil {
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// start read file from request
+	file, _, err := r.FormFile(UploadFormFileKey)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	var opportunitesFromFile []Opportunity
+	readOpportunitiesFromFile(file, &opportunitesFromFile)
+	// end read file from request
+
+	// blocks that will be updated
+	var allUserBlocks []*model.Block
+	var userCardBlocks []*model.Block
+
+	// retrieve all existing blocks from the board
+	allUserBlocks, err = a.app.GetBlocks(boardID, "", "card")
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+
+	templateBlockID := "noch keine ID gefunden"
+
+	for i := 0; i < len(allUserBlocks); i++ {
+		if allUserBlocks[i].Fields["isTemplate"] == true && allUserBlocks[i].Title == "Neue Ausschreibung" {
+			templateBlockID = allUserBlocks[i].ID
+			break
+		}
+	}
+
+	for i := 0; i < len(allUserBlocks); i++ {
+		if allUserBlocks[i].Fields["isTemplate"] == false {
+			userCardBlocks = append(userCardBlocks, allUserBlocks[i])
+		}
+	}
+
+	opportunitesFromFile = opportunitesFromFile[:5]
+
+	boardTemplate, err := a.app.GetBoard(boardID)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+
+	// Check if the board contains all the master client names needed for opportunities
+	masterClientNameOptions := selectPossibleValueById(masterClientNameKey, boardTemplate.CardProperties)
+	fmt.Println("Length of Master Client Name Options: ", len(masterClientNameOptions))
+	fmt.Println("Master Client Name Options: ", masterClientNameOptions)
+
+	for _, opportunity := range opportunitesFromFile {
+		var newOptions []map[string]interface{}
+		newOptions = searchForNearestPossibleOptionAndModifyIfItDoesntExist(opportunity.MasterClientName, masterClientNameOptions)
+
+		masterClientNameOptions = newOptions
+	}
+
+	fmt.Println("Length of new Optionen: ", len(masterClientNameOptions))
+	fmt.Println("Neue Optionen: ", masterClientNameOptions)
+
+	var patchBoard *model.BoardPatch = &model.BoardPatch{
+		UpdatedCardProperties: []map[string]interface{}{
+			{
+				"id":      masterClientNameKey,
+				"name":    "Kunde",
+				"options": masterClientNameOptions,
+				"type":    "select",
+			},
+		},
+	}
+
+	a.app.PatchBoard(patchBoard, boardID, userID)
+	newBoardTemplate, err := a.app.GetBoard(boardID)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+
+	boardTemplate = newBoardTemplate
+
+	_ = templateBlockID
+
+	var updatedBlocks []*model.Block
+	updatedBlocks = a.filterBlocksByOpportunityId(userCardBlocks, &opportunitesFromFile, boardTemplate, templateBlockID, userID)
+
+	tempVar, err := a.app.InsertBlocksAndNotify(updatedBlocks, userID, true)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	} else {
+
+	}
+
+	_ = tempVar
+
+	// if templateBlockID != "noch keine ID gefunden" {
+	// 	for i := 0; i < len(opportunitesFromFile); i++ {
+	// 		duplicateBlocks, err := a.app.DuplicateBlock(boardID, templateBlockID, userID, true)
+	// 		if err != nil {
+	// 			return
+	// 		}
+
+	// 		_ = duplicateBlocks
+
+	// 		var singleDuplicateBlock *model.Block
+	// 		singleDuplicateBlock = duplicateBlocks[0]
+	// 		convertOpportunityToBlock(opportunitesFromFile[i], singleDuplicateBlock)
+
+	// 		blocks = append(blocks, singleDuplicateBlock)
+	// 	}
+	// }
+
+	// fmt.Println(blocks)
+
+	// tempVar, err := a.app.InsertBlocksAndNotify(blocks, userID, true)
+	// if err != nil {
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// _ = tempVar
+
+	// auditRec := a.makeAuditRecord(r, "import", audit.Fail)
+	// defer a.audit.LogRecord(audit.LevelModify, auditRec)
+	// auditRec.AddMeta("filename", handle.Filename)
+	// auditRec.AddMeta("size", handle.Size)
+
+	// opt := model.ImportArchiveOptions{
+	// 	TeamID:     teamID,
+	// 	ModifiedBy: userID,
+	// }
+
+	// if err := a.app.ImportArchive(file, opt); err != nil {
+	// 	a.logger.Debug("Error importing archive",
+	// 		mlog.String("team_id", teamID),
+	// 		mlog.Err(err),
+	// 	)
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// abc := ReturnTypeABC{opportunityId: opportunityIdArr, blocks: blocks}
+
+	// fmt.Println(abc.blocks, abc.opportunityId)
+
+	// var bErr error
+	// blocks, bErr = a.app.ApplyCloudLimits(blocks)
+	// if bErr != nil {
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// json, err := json.Marshal(blocks)
+	// if err != nil {
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// jsonBytesResponse(w, http.StatusOK, json)
+
+	// var blocksa []*model.Block
+	// var newBlock *model.Block
+	// newBlock = &model.Block{}
+
+	// blocksa = append(blocksa, newBlock)
+
+	// hasComments := false
+	// hasContents := false
+	// for _, block := range blocksa {
+	// 	block.BoardID = boardID
+	// 	block.Type = "card"
+	// 	block.Fields["properties"] = map[string]interface{}{"opportunityId": "Neue ID"}
+	// }
+
+	// if hasContents {
+	// 	if !a.permissions.HasPermissionToBoard(userID, boardID, model.PermissionManageBoardCards) {
+	// 		a.errorResponse(w, r, model.NewErrPermission("access denied to make board changes"))
+	// 		return
+	// 	}
+	// }
+	// if hasComments {
+	// 	if !a.permissions.HasPermissionToBoard(userID, boardID, model.PermissionCommentBoardCards) {
+	// 		a.errorResponse(w, r, model.NewErrPermission("access denied to post card comments"))
+	// 		return
+	// 	}
+	// }
+
+	// blocksa = model.GenerateBlockIDs(blocksa, a.logger)
+
+	// // a.app.PatchBlock()
+
+	// newBlocks, err := a.app.InsertBlocksAndNotify(blocks, userID, true)
+	// if err != nil {
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// _ = newBlocks
+
+	// data, err := json.Marshal(abc)
+	// if err != nil {
+	// 	a.errorResponse(w, r, err)
+	// 	return
+	// }
+
+	// jsonBytesResponse(w, http.StatusOK, data)
+	// auditRec.Success()
+}
+
+func (a *API) handleArchiveImport(w http.ResponseWriter, r *http.Request) {
+	// swagger:operation POST /teams/{teamID}/archive/import archiveImport
+	//
+	// Import an archive of boards.
+	//
+	// ---
+	// produces:
+	// - application/json
+	// consumes:
+	// - multipart/form-data
+	// parameters:
+	// - name: teamID
+	//   in: path
+	//   description: Team ID
+	//   required: true
+	//   type: string
+	// - name: file
+	//   in: formData
+	//   description: archive file to import
+	//   required: true
+	//   type: file
+	// security:
+	// - BearerAuth: []
+	// responses:
+	//   '200':
+	//     description: success
+	//   default:
+	//     description: internal error
+	//     schema:
+	//       "$ref": "#/definitions/ErrorResponse"
+
+	ctx := r.Context()
+	session, _ := ctx.Value(sessionContextKey).(*model.Session)
+	userID := session.UserID
+
+	vars := mux.Vars(r)
+	teamID := vars["teamID"]
+
+	if !a.permissions.HasPermissionToTeam(userID, teamID, model.PermissionViewTeam) {
+		a.errorResponse(w, r, model.NewErrPermission("access denied to create board"))
+		return
+	}
+
+	isGuest, err := a.userIsGuest(userID)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+	if isGuest {
+		a.errorResponse(w, r, model.NewErrPermission("access denied to create board"))
+		return
+	}
+
+	file, handle, err := r.FormFile(UploadFormFileKey)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	defer file.Close()
+
+	auditRec := a.makeAuditRecord(r, "import", audit.Fail)
+	defer a.audit.LogRecord(audit.LevelModify, auditRec)
+	auditRec.AddMeta("filename", handle.Filename)
+	auditRec.AddMeta("size", handle.Size)
+
+	opt := model.ImportArchiveOptions{
+		TeamID:     teamID,
+		ModifiedBy: userID,
+	}
+
+	if err := a.app.ImportArchive(file, opt); err != nil {
+		a.logger.Debug("Error importing archive",
+			mlog.String("team_id", teamID),
+			mlog.Err(err),
+		)
+		a.errorResponse(w, r, err)
+		return
+	}
+
+	jsonStringResponse(w, http.StatusOK, "{}")
+	auditRec.Success()
+}
+
+func (a *API) handleArchiveExportTeam(w http.ResponseWriter, r *http.Request) {
+	// swagger:operation GET /teams/{teamID}/archive/export archiveExportTeam
+	//
+	// Exports an archive of all blocks for all the boards in a team.
+	//
+	// ---
+	// produces:
+	// - application/json
+	// parameters:
+	// - name: teamID
+	//   in: path
+	//   description: Id of team
+	//   required: true
+	//   type: string
+	// security:
+	// - BearerAuth: []
+	// responses:
+	//   '200':
+	//     description: success
+	//     content:
+	//       application-octet-stream:
+	//         type: string
+	//         format: binary
+	//   default:
+	//     description: internal error
+	//     schema:
+	//       "$ref": "#/definitions/ErrorResponse"
+	if a.MattermostAuth {
+		a.errorResponse(w, r, model.NewErrNotImplemented("not permitted in plugin mode"))
+		return
+	}
+
+	vars := mux.Vars(r)
+	teamID := vars["teamID"]
+
+	ctx := r.Context()
+	session, _ := ctx.Value(sessionContextKey).(*model.Session)
+	userID := session.UserID
+
+	auditRec := a.makeAuditRecord(r, "archiveExportTeam", audit.Fail)
+
+	defer a.audit.LogRecord(audit.LevelRead, auditRec)
+	auditRec.AddMeta("TeamID", teamID)
+
+	isGuest, err := a.userIsGuest(userID)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+
+	boards, err := a.app.GetBoardsForUserAndTeam(userID, teamID, !isGuest)
+	if err != nil {
+		a.errorResponse(w, r, err)
+		return
+	}
+	ids := []string{}
+	for _, board := range boards {
+		ids = append(ids, board.ID)
+	}
+
+	opts := model.ExportArchiveOptions{
+		TeamID:   teamID,
+		BoardIDs: ids,
+	}
+
+	filename := fmt.Sprintf("archive-%s%s", time.Now().Format("2006-01-02"), archiveExtension)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+
+	if err := a.app.ExportArchive(w, opts); err != nil {
+		a.errorResponse(w, r, err)
+	}
+
+	auditRec.Success()
+}
---
+++ server/api/import_opportunity_utils.go
@@ -0,0 +1,252 @@
+package api
+
+import (
+	"fmt"
+	"mime/multipart"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mattermost/focalboard/server/model"
+	mmModel "github.com/mattermost/mattermost-server/v6/model"
+	"github.com/xuri/excelize/v2"
+)
+
+type Opportunity struct {
+	OpportunityId                string
+	ReportingStatus              string
+	FYQtr                        string
+	CG                           string
+	MasterClientName             string
+	OpportunityName              string
+	OpportunityDetailDescription string
+	IndustrySegment              string
+	Stage                        string
+	TotalCurrentRevenue          string
+	WinProbability               string
+	SellingCountry               string
+	Level2                       string
+	SalesCapture                 string
+	CreateDate                   string
+	Revenue                      string
+}
+
+const (
+	opportunityIdKey       = "az4n94b4qs14czh8oz65iy5oqch"
+	stageKey               = "a5hwxjsmkn6bak6r7uea5bx1kwc"
+	masterClientNameKey    = "ainpw47babwkpyj77ic4b9zq9xr"
+	totalCurrentRevenueKey = "a4fbeujhoww8rhqh8jdw339nukh"
+	winProbabilityKey      = "af7odq9ibb9f8rmap7wtdje5yua"
+	salesCaptureKey        = "auoepp7uiphkaypcx3k34icuoxa"
+	cgKey                  = "af1dyxxzjh8ycihtw6nx1tb7mty"
+	level2Key              = "a3ait3cuckgx19pshzxyyx758fe"
+	createDateKey          = "ay3wkiaubujrfe4t3cg8y4gr4kh"
+)
+
+func readOpportunitiesFromFile(file multipart.File, opportunities *[]Opportunity) {
+	f, err := excelize.OpenReader(file)
+	if err != nil {
+		fmt.Println("Fehler!!!")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	rows, err := f.GetRows("MMS Data")
+	_ = rows
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Skip the first 5 rows
+	rows = rows[5:]
+
+	for i := 0; i < len(rows); i++ {
+		opportunityId := rows[i][1]
+		reportingStatus := rows[i][2]
+		fyQtr := rows[i][3]
+		cg := rows[i][4]
+		masterClientName := rows[i][5]
+		opportunityName := rows[i][6]
+		opportunityDetailDescription := rows[i][7]
+		industrySegment := rows[i][8]
+		stage := rows[i][9]
+		totalCurrentRevenue := rows[i][10]
+		winProbability := rows[i][11]
+		sellingCountry := rows[i][12]
+		level2 := rows[i][13]
+		salesCapture := rows[i][14]
+		createDate := rows[i][15]
+		revenue := rows[i][16]
+
+		opportunity := Opportunity{
+			OpportunityId:                opportunityId,
+			ReportingStatus:              reportingStatus,
+			FYQtr:                        fyQtr,
+			CG:                           cg,
+			MasterClientName:             masterClientName,
+			OpportunityName:              opportunityName,
+			OpportunityDetailDescription: opportunityDetailDescription,
+			IndustrySegment:              industrySegment,
+			Stage:                        stage,
+			TotalCurrentRevenue:          totalCurrentRevenue,
+			WinProbability:               winProbability,
+			SellingCountry:               sellingCountry,
+			Level2:                       level2,
+			SalesCapture:                 salesCapture,
+			CreateDate:                   createDate,
+			Revenue:                      revenue,
+		}
+
+		*opportunities = append(*opportunities, opportunity)
+	}
+}
+
+func selectPossibleValueById(id string, possibleValues []map[string]interface{}) []map[string]interface{} {
+	for _, value := range possibleValues {
+		if value["id"] == id {
+			var options = value["options"]
+			v := reflect.ValueOf(options).Interface().([]interface{})
+
+			var returnArray []map[string]interface{}
+
+			for _, item := range v {
+				returnArray = append(returnArray, item.(map[string]interface{}))
+			}
+
+			return returnArray
+			// return value["options"].([]Item)
+			// } else {
+			// 	fmt.Println("Unable to convert options to []Item")
+			// 	fmt.Println("option: ", value["options"])
+			// 	return nil
+			// }
+		}
+	}
+
+	fmt.Println("No possible value found for id: " + id)
+
+	return nil
+}
+
+func searchForNearestPossibleOption(value string, options []map[string]interface{}) string {
+	for _, possibleValue := range options {
+		if strings.Contains(strings.ToLower(possibleValue["value"].(string)), strings.ToLower(value)) {
+			return possibleValue["id"].(string)
+		}
+	}
+
+	return ""
+}
+
+func searchForNearestPossibleOptionAndModifyIfItDoesntExist(value string, options []map[string]interface{}) []map[string]interface{} {
+	newOptions := options
+	found := searchForNearestPossibleOption(value, newOptions)
+
+	if found == "" {
+		// If we don't find a match, we create a new option
+		newOption := map[string]interface{}{
+			"id":    mmModel.NewId(),
+			"value": value,
+			"color": "propColorDefault",
+		}
+
+		newOptions = append(newOptions, newOption)
+	}
+
+	return newOptions
+}
+
+func convertDateStringToDateObject(dateString string) string {
+	if dateString != "" {
+		// Truncate the extra precision
+		dateString = dateString[:30] + "Z"
+
+		// Parse the date string
+		t, err := time.Parse(time.RFC3339Nano, dateString)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Convert to epoch timestamp
+		epoch := t.UnixNano() / int64(time.Millisecond)
+		var dateObject = "{\"from\":" + strconv.FormatInt(epoch, 10) + "}"
+
+		// structure for output: "{\"from\":1706788800000}"
+		// "{\"from\":1706788800000}"
+		return dateObject
+	}
+
+	return ""
+}
+
+func convertOpportunityToBlock(opportunity Opportunity, block *model.Block, possibleValues []map[string]interface{}) {
+	// block := model.Block{
+	// 	Title:     opportunity.OpportunityName,
+	// 	BoardID:   boardID,
+	// 	Type:      "card",
+	// 	CreatedBy: userID,
+	// 	ParentID:  "bjz11k7xjopb8fyaqppzog3uuqr",
+	// }
+
+	stage := searchForNearestPossibleOption(opportunity.Stage, selectPossibleValueById(stageKey, possibleValues))
+	cg := searchForNearestPossibleOption(opportunity.CG, selectPossibleValueById(cgKey, possibleValues))
+	date := convertDateStringToDateObject(opportunity.CreateDate)
+	masterClientName := searchForNearestPossibleOption(opportunity.MasterClientName, selectPossibleValueById(masterClientNameKey, possibleValues))
+
+	fmt.Println("Stage original value: ", opportunity.Stage)
+	fmt.Println("Stage for ", opportunity.OpportunityName, ": ", stage)
+	fmt.Println("CG original value: ", opportunity.CG)
+	fmt.Println("CG for ", opportunity.OpportunityName, ": ", cg)
+	fmt.Println("Date original value: ", opportunity.CreateDate)
+	fmt.Println("Date for ", opportunity.OpportunityName, ": ", date)
+	fmt.Println("MasterClientName original value: ", opportunity.MasterClientName)
+	fmt.Println("MasterClientName for ", opportunity.OpportunityName, ": ", masterClientName)
+
+	block.Title = opportunity.OpportunityName
+	block.Fields["isTemplate"] = false
+	// TODO rework this so old information doesn't get overwritten
+	if _, ok := block.Fields["properties"]; ok {
+		// "properties" exists, so we cast it to the appropriate type and add the new values
+		properties := block.Fields["properties"].(map[string]interface{})
+		properties[opportunityIdKey] = opportunity.OpportunityId
+		properties[stageKey] = stage
+		properties[masterClientNameKey] = masterClientName
+		properties[totalCurrentRevenueKey] = opportunity.TotalCurrentRevenue
+		properties[winProbabilityKey] = opportunity.WinProbability
+		properties[salesCaptureKey] = opportunity.SalesCapture
+		properties[cgKey] = cg
+		properties[level2Key] = opportunity.Level2
+		properties[createDateKey] = date
+	} else {
+		// "properties" does not exist, so we create a new map and add the values to it
+		block.Fields["properties"] = map[string]interface{}{
+			opportunityIdKey:       opportunity.OpportunityId,
+			stageKey:               stage,
+			masterClientNameKey:    masterClientName,
+			totalCurrentRevenueKey: opportunity.TotalCurrentRevenue,
+			winProbabilityKey:      opportunity.WinProbability,
+			salesCaptureKey:        opportunity.SalesCapture,
+			cgKey:                  cg,
+			level2Key:              opportunity.Level2,
+			createDateKey:          date,
+		}
+	}
+}
+
+// option:  [
+// 	map[color:propColorGreen id:akj61wc9yxdwyw3t6m8igyf9d5o value:Stage 0]
+// 	map[color:propColorGray id:aic89a5xox4wbppi6mbyx6ujsda value:Stage 1]
+// 	map[color:propColorOrange id:ah6ehh43rwj88jy4awensin8pcw value:Stage 2a]
+// 	map[color:propColorRed id:aprhd96zwi34o9cs4xyr3o9sf3c value:Stage 2b]
+// 	map[color:propColorPurple id:axesd74yuxtbmw1sbk8ufax7z3a value:WON 🏆]
+// 	map[color:propColorDefault id:a5txuiubumsmrs8gsd5jz5gc1oa value:LOST]
+// 	map[color:propColorBrown id:acm9q494bcthyoqzmfogxxy5czy value:REQUEST]
+// 	map[color:propColorDefault id:a6n6aqi7k65tiqtr5gdzfbosmcw value:Tigital]
+// 	map[color:propColorDefault id:ai6u8x1t9fq1mckj8ph1tgfn5uw value:EXT]
+// 	map[color:propColorDefault id:aqh8bu8c4ai5broqqnf45ufm5kh value:Stage 3A]
+// 	map[color:propColorDefault id:afx6sqzc5q5mmnrs6wpzgx3fwca value:Stage 3B]
+// 	map[color:propColorDefault id:af47cinzyk96pufqhsjg84zjo6c value:Stage 3C]
+// ]
